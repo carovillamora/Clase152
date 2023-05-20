25bf508f62b440b3a7cf4da463fcc3f3f30d1f43
carovillamora <94028506+carovillamora@users.noreply.github.com>
2023-05-20 17:17:07 -0600

    Add files via upload

---
+++ Move-Position.js
@@ -0,0 +1,32 @@
+// Registrando componentes
+AFRAME.registerComponent("move", {
+  schema: {
+    
+  },
+
+  tick: function () {
+    
+  },
+});
+
+AFRAME.registerComponent("camera-zoom-out", {
+  schema: {
+    
+  },
+
+  tick: function () {
+    
+  },
+});
+
+AFRAME.registerComponent("fall-down", {
+  schema: {
+    
+  },
+  tick: function () {
+
+    
+  }
+});
+
+
